a80f76e808df37a0b0d49a071e72c0191de0e14d
JKamue <mail@jkamue.de>
2019-08-07 20:35:21 +0200

    Set basic functions of map object

--- lib/classes/Map.php
+++ lib/classes/Map.php
@@ -3,9 +3,28 @@
 
 class Map
 {
+    public static function getNewId() : Int
+    {
+        $id = file_get_contents(ROOT . "data/tmp/mapcounter.txt");
+        $id++;
+        file_put_contents(ROOT . "data/tmp/mapcounter.txt", $id);
+        return $id;
+    }
+
     public static function getOrCreate(String $name): Map
     {
-        // TODO Look if map exists and Generate Map object
+        $sql = "SELECT MapID FROM map WHERE MapName = ?";
+        $map_data = Database::execute($sql, array($name));
+
+        if ($map_data->rowCount() != 1) {
+            $id = self::getNewId();
+            $map = new Map($id, $name);
+            $map->save();
+        } else {
+            $map_data = $map_data->fetchAll()[0];
+            $map = new Map($map_data['id'], $name);
+        }
+        return $map;
     }
 
     private $name;
@@ -17,15 +36,19 @@ class Map
         $this->name = $name;
     }
 
-    private function id() : String {
+    private function id() : String
+    {
         return $this->id;
     }
 
-    private function name() : String {
+    private function name() : String
+    {
         return $this->name;
     }
 
-    public function save() {
-        // Todo implement
+    private function save()
+    {
+        $sql = "INSERT INTO map (MapID, MapName) VALUES  (?, ?)";
+        Database::execute($sql, array($this->id, $this->name));
     }
 }
