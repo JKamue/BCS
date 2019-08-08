5049ab79cc590f34abd28781278af7fdb79a8619
JKamue <mail@jkamue.de>
2019-08-08 21:14:12 +0200

    Finished lineup class

--- lib/classes/Lineup.php
+++ lib/classes/Lineup.php
@@ -12,10 +12,11 @@ class Lineup
 
     public static function createFromArray($clan, $members) : Lineup
     {
+        sort($members);
         return self::create($clan,  $members[0], $members[1], $members[2], $members[3]);
     }
 
-    public static function create(Clan $clan, Member $member1, Member $member2, Member $member3, Member $member4) : Lineup
+    private static function create(Clan $clan, Member $member1, Member $member2, Member $member3, Member $member4) : Lineup
     {
         return new Lineup($clan, $member1, $member2, $member3, $member4);
     }
@@ -40,5 +41,16 @@ class Lineup
             $this->member3->id().
             $this->member4->id()
         );
+    }
+
+    public function save()
+    {
+        $sql = "SELECT ClanUUID FROM lineup WHERE LineupID = ?";
+        $count = Database::count($sql, array($this->id));
+
+        if ($count != 1) {
+            $sql = "INSERT INTO lineup (LineupID, ClanUUID, Player1UUID, Player2UUID, Player3UUID, Player4UUID) VALUES (?, ?, ?, ?, ?, ?)";
+            Database::execute($sql, array($this->id, $this->clan, $this->member1, $this->member2, $this->member3, $this->member4));
+        }
     }
 }
