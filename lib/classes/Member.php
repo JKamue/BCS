0b733449b6c73af68ac24346d3a870b8e5d94d91
JKamue <mail@jkamue.de>
2019-08-08 19:44:18 +0200

    Set basic functions of member object

--- lib/classes/Member.php
+++ lib/classes/Member.php
@@ -19,21 +19,59 @@ class Member
     public $clan;
     public $player;
 
-    public static function getOrCreate(Clan $clan, Player $player): Member
+    public static function create(Clan $clan, Player $player) : Member
     {
-        // Todo calculate id and construct
+        $id = md5($clan->id() . $player->id());
+
+        $active = 1;
+        $mvp = $beds = $kills = $killed = $quits = $died = $bac = 0;
+
+        return new Member($id, $active, $mvp, $beds, $kills, $killed, $quits, $died, $bac, $clan, $player);
     }
 
-    private function __construct(Clan $clan, Player $player)
+    private function __construct(String $id, Int $active, Int $mvp, Int $beds, Int $kills, Int $killed, Int $quits, Int $died, $bac, Clan $clan, Player $player)
     {
-
+        $this->id = $id;
+        $this->active = $active;
+        $this->mvp = $mvp;
+        $this->beds = $beds;
+        $this->kills = $kills;
+        $this->killed = $killed;
+        $this->quits = $quits;
+        $this->died = $died;
+        $this->bac = $bac;
+        $this->clan = $clan;
+        $this->player = $player;
     }
 
     public function id() : String {
         return $this->id();
     }
 
-    public function save() {
-        // Todo implement
+    public function save()
+    {
+        $sql = "SELECT UUID FROM member WHERE PlayerID = ?";
+        $stm = Database::execute($sql, array($this->id));
+
+        if ($stm->rowCount() == 1) {
+            $sql = "UPDATE member SET 
+                  Active = ?,
+                  MVP = MVP + ?,
+                  Kills = Kills + ?,
+                  Killed = Killed + ?,
+                  Quits = Quits + ?,
+                  Died = Died + ?,
+                  BAC = BAC + ?
+                WHERE PlayerID = ?";
+            $data = array($this->active, $this->mvp, $this->kills, $this->killed, $this->quits, $this->died, $this->bac, $this->id);
+        } else {
+            $this->player->save();
+
+            $sql = "INSERT INTO member 
+                (PlayerID, Active, MVP, Betten, Kills, Killed, Quits, Died, ClanUUID, UUID, BAC) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $data = array($this->id, $this->active, $this->mvp, $this->beds, $this->kills, $this->killed, $this->quits, $this->died, $this->clan->id(), $this->player->id(), $this->bac);
+        }
+        Database::execute($sql, $data);
     }
 }
