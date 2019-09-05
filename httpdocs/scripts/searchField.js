a18e0b7d48b777a770d890e59e703b2ad85043f3
JKamue <mail@jkamue.de>
2019-09-05 18:17:05 +0200

    Optimized search

--- httpdocs/scripts/searchField.js
+++ httpdocs/scripts/searchField.js
@@ -1,13 +1,7 @@
-function searchForClans() {
-	var val = document.getElementById("searchField").value;
+function searchForClans(id) {
+	var val = document.getElementById(id).value;
 	
 	if ( val != "" && val != null) {
-		window.location = "search.html?search=" + val;
-	}
-	
-	var val2 = document.getElementById("searchField2").value;
-	
-	if ( val2 != "" && val2 != null) {
 		window.location = "search.html?search=" + val;
 	}
 }
